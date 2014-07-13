1fad167ffb853bf2b3c6c4604a4eb2d7a361f8bd
Masato Hori <rillomas@gmail.com>
2014-07-14 00:31:19 +0900

    changed constructor to the new polymer style

--- lib/components/custom_list_item.dart
+++ lib/components/custom_list_item.dart
@@ -6,7 +6,9 @@ import 'package:custom_list/view_models/view_models.dart';
 /// A custom list item
 @CustomTag("custom-list-item")
 class CustomListItem extends LIElement with Polymer, Observable {
-  CustomListItem.created(): super.created();
+  CustomListItem.created(): super.created() {
+    polymerCreated();
+  }
 
   @published
   ItemViewModel item;
